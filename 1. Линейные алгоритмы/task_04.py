a29feb5e0280d60217dcdd0e2a2bccb57aca3559
Ruslan <itachi_7@mail.ru>
2025-04-27 17:26:49 +0300

    Add files via upload

---
+++ 1. Линейные алгоритмы/task_04.py
@@ -0,0 +1,16 @@
+# С клавиатуры вводятся границы числового диапазона. Получите случайное целое
+# число в его пределах и выведите его на экран.
+
+from random import randrange, randint
+
+number1 = int(input('Введите нижнюю границу: '))
+number2 = int(input('Введите верхнюю границу: '))
+
+result = randint(number1, number2)
+
+print(result)
+
+# чтобы верхняя граница не входила
+
+n = randrange(number1, number2)
+print(n)
